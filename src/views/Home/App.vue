cb44af31405bd9dbd979714ed7ed9980362b46af
mathms01 <mathieu.mussot01@gmail.com>
2023-01-06 00:54:51 +0100

    [ADD] paralax on home

--- src/views/Home/App.vue
+++ src/views/Home/App.vue
@@ -1,18 +1,23 @@
 <template>
-<v-container fluid class="fill-height">
-  <v-container class="banner">
-    <v-row>
-      <v-col cols="1"></v-col>
-      <v-col cols="3">
-        <HomePres/>
-      </v-col>
-      <v-col cols="1"></v-col>
-      <v-col cols="7">
-        <CarrousselProjects/>
-      </v-col>
-    </v-row>
-  </v-container>
   <v-container>
+    <v-parallax
+      dark
+      src="http://drive.google.com/uc?export=view&id=1TkU7smZTUsuI9kciuOg4xkoCzYINFSG5"
+      class="v-parallax__image"
+    >
+    </v-parallax>
+      <v-row>
+        <v-col cols="1"></v-col>
+        <v-col cols="3">
+          <HomePres/>
+        </v-col>
+        <v-col cols="1"></v-col>
+        <v-col cols="7">
+          <CarrousselProjects/>
+        </v-col>
+      </v-row>
+  </v-container>
+<v-container fluid class="fill-height">
     <v-row>
       <FreelancePresVue></FreelancePresVue>
     </v-row>
@@ -22,7 +27,6 @@
     <v-row>
       <TechnoPresVue></TechnoPresVue>
     </v-row>
-  </v-container>
 </v-container>
 </template>
 
@@ -56,9 +60,19 @@ export default {
 }
 
 .banner {
-  background-image: url(https://dge4uaysoh8oy.cloudfront.net/lp/creatopy/624/images/1630069165764_CTA---Blueberry---5.jpg);
   background-size:cover;
-  height: 500px;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+}
+
+.v-parallax__image {
+  transform: scale(1.6) !important;
+  width: 100% !important;
+  height: 20% !important;
+  position: absolute;
 }
 
 </style>
